8e1dce043204919f016eaffd56e46084f91413d6
Jongkeun21 <whdrms5560@gmail.com>
2023-10-13 10:07:19 +0900

    add new sphinx repo

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'bigdata-car'
+copyright = '2023, jklee2'
+author = 'jklee2'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+extensions = ['sphinx.ext.autodoc', 'myst_parser']
+html_theme = 'sphinx_rtd_theme'
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_static_path = ['_static']
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
---
+++ src/test.py
@@ -0,0 +1,16 @@
+"""
+This is a test page of bigdata-car(KADaP) using sphinx.
+"""
+
+class MyTest:
+    """
+    Sample class
+    """
+
+    def myfunc(self, param1) :
+        '''
+        Sample function
+        :param1: Sample param
+        :return:
+        '''
+        return param1
